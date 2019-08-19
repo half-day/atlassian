03dad017bcb39850bff2ea94c0dd343e795d3c42
Andre Kolodochka <andre@andrek.com>
2019-08-19 13:07:12 +1000

    Initial commit

--- directories.sh
+++ directories.sh
@@ -1,1 +1,24 @@
-mkdir /opt/atlassian
+#!/bin/bash
+
+# Creating Atlassian applications and data directory structure
+cd /opt/atlassian/
+mkdir apps
+mkdir data
+cd /opt/atlassian/apps
+mkdir jira
+mkdir confluence
+mkdir crowd
+cd /opt/atlassian/data
+mkdir jira
+mkdir confluence
+mkdir crowd
+
+#creating 'atlassian' username all applications will run under
+useradd atlassian
+
+#changing directories ownership to 'atlassian' user
+cd /opt
+chown -R atlassian:atlassian atlassian
+
+# Creating Java directory
+mkdir /opt/java
